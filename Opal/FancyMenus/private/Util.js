9b07356eb7bf81f84f0ccebb253fe905867aa55b
roundedrectangle <kozelderezel12345678@gmail.com>
2025-06-22 22:09:51 +0300

    Util.js: findHighlightRectangle: make it slightly faster

--- Opal/FancyMenus/private/Util.js
+++ Opal/FancyMenus/private/Util.js
@@ -1,15 +1,12 @@
 function findHighlightRectangle(item) {
-    var bar
     // find highlight rectangle
     for (var i=0; i<item.data.length; i++) {
         var childItem = item.data[i]
         if (childItem.hasOwnProperty("highlightedItem"))
             // got the highlightbar
-            bar = childItem
+            return childItem
     }
-    if (!bar)
-        console.log("HighlightBar not found!")
-    return bar
+    console.log("HighlightBar not found!")
 }
 
 function childAt(parent, x, y) {
